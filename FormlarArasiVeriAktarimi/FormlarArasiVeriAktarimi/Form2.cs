7f168958f8d89b4e27dc89f8c808e3b0603b794f
kubrataskir <127491457+kubrataskir@users.noreply.github.com>
2023-06-19 20:44:40 +0300

    Add files via upload

---
+++ FormlarArasiVeriAktarimi/FormlarArasiVeriAktarimi/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasiVeriAktarimi
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string kullaniciAdi;
+        public string sifre;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = kullaniciAdi;
+            label2.Text=sifre;
+        }
+    }
+}
